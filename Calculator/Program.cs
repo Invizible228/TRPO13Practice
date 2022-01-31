1d2b3bc8d974e353aa81917b26b5fa8245630a35
Invizible228 <Sulaymanov_tima@mail.ru>
2022-01-31 11:36:32 +0500

    mas

---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Выберите цифру:\n1 - '+'\n2 - '-'\n3 - '*'\n4 - '/'\nВаш вариант: ");
+int lever = Convert.ToInt32(Console.ReadLine());
+switch (lever)
+{
+    
+}
